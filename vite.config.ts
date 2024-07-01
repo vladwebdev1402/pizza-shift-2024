3a743c8cca500401ba48cd0f0e1cbf0ae70efbed
vladwebdev1402 <mmovlad.kriven.1402@gmail.com>
2024-07-02 01:24:02 +0700

    init project

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
+import react from '@vitejs/plugin-react';
+import path from 'node:path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), svgr()],
+  server: { port: 3000 },
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, './src/') }],
+  },
+});

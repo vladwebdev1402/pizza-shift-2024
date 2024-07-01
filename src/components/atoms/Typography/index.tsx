2e4b2fc152983af09e3b46ab810a0ab494b3cbdf
vladwebdev1402 <mmovlad.kriven.1402@gmail.com>
2024-07-02 03:22:16 +0700

    add typography

---
+++ src/components/atoms/Typography/index.tsx
@@ -0,0 +1,48 @@
+import { FC, ReactNode } from 'react';
+import { clsx } from 'clsx';
+
+import style from './style.module.scss';
+
+type TypographyVariants =
+  | 'h2'
+  | 'h3'
+  | 'paragraph_16'
+  | 'paragraph_14'
+  | 'paragraph_12';
+
+type TypographyColors =
+  | 'title'
+  | 'invert'
+  | 'primary'
+  | 'secondary'
+  | 'tertiary'
+  | 'quartenery'
+  | 'body-primary';
+
+type TypographyWeights = 'regular' | 'medium' | 'semibold' | 'bold';
+
+type Props = {
+  variant?: TypographyVariants;
+  color?: TypographyColors;
+  weight?: TypographyWeights;
+  tag?: 'h1' | 'h2' | 'h3' | 'p' | 'span' | 'div';
+  children: ReactNode;
+};
+
+const Typography: FC<Props> = ({
+  variant = 'paragraph_16',
+  color = 'primary',
+  weight = 'regular',
+  tag = 'p',
+  children,
+}) => {
+  const Tag = tag;
+
+  return (
+    <Tag className={clsx(style[variant], style[color], style[weight])}>
+      {children}
+    </Tag>
+  );
+};
+
+export { Typography };
--- src/components/pages/MainPage/index.tsx
+++ src/components/pages/MainPage/index.tsx
@@ -1,13 +1,5 @@
-import { Button } from '@/components/atoms';
-
 const MainPage = () => {
-  return (
-    <div>
-      <Button>aasdasda</Button>
-      <Button variant="outlined">outlined</Button>
-      <Button variant="text">text</Button>
-    </div>
-  );
+  return <div></div>;
 };
 
 export { MainPage };

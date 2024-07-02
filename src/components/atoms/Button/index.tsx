8ca8d39d3a0e1b8696241d1864e886d9819cb9b0
vladwebdev1402 <mmovlad.kriven.1402@gmail.com>
2024-07-02 18:24:10 +0700

    refactoring button

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -1,23 +1,44 @@
-import { ComponentProps, FC } from 'react';
+import { ComponentProps, FC, ReactNode } from 'react';
 import clsx from 'clsx';
 
 import style from './style.module.scss';
+import { Loader } from '../Loader';
 
-type Props = {
+type ButtonProps = {
   variant?: 'contained' | 'outlined' | 'text';
+  iconPosition?: 'center' | 'start';
+  fullWidth?: boolean;
+  loading?: boolean;
+  icon?: ReactNode;
 } & ComponentProps<'button'>;
 
-const Button: FC<Props> = ({ variant = 'contained', children, ...props }) => {
+const Button: FC<ButtonProps> = ({
+  variant = 'contained',
+  iconPosition = 'center',
+  fullWidth = false,
+  loading = false,
+  className = '',
+  icon,
+  children,
+  disabled,
+  ...props
+}) => {
   return (
     <button
       {...props}
-      className={clsx(style.button, {
+      disabled={disabled || loading}
+      className={clsx(className, style.button, {
         [style.button_contained]: variant === 'contained',
         [style.button_outlined]: variant === 'outlined',
         [style.button_text]: variant === 'text',
+        [style.button_fullWidth]: fullWidth,
+        [style.button_iconCenter]: iconPosition === 'center',
+        [style.button_iconStart]: iconPosition === 'start',
       })}
     >
-      {children}
+      {loading && <Loader />}
+      {!loading && icon && <span className={style.icon}>{icon}</span>}
+      {!loading && children}
     </button>
   );
 };
--- src/components/pages/MainPage/index.tsx
+++ src/components/pages/MainPage/index.tsx
@@ -1,18 +1,25 @@
-import { Container, NavLink } from '@/components/atoms';
-import { ROUTER_PATHS } from '@/constants';
-import TimeSVG from '@/assets/decorative/time.svg?react';
+import { Button } from '@/components/atoms';
+
 import ProfileSVG from '@/assets/decorative/profile.svg?react';
 
 const MainPage = () => {
   return (
-    <Container>
-      <NavLink type="footer" to={ROUTER_PATHS.main} icon={<TimeSVG />}>
-        Главная
-      </NavLink>
-      <NavLink type="footer" to={'/adsads'} icon={<ProfileSVG />}>
-        Профиль
-      </NavLink>
-    </Container>
+    <>
+      <Button icon={<ProfileSVG />} iconPosition="start" fullWidth>
+        Тест кнопка иконка
+      </Button>
+      <Button
+        icon={<ProfileSVG />}
+        iconPosition="center"
+        variant="outlined"
+        fullWidth
+      >
+        Тест кнопка иконка
+      </Button>
+      <Button loading fullWidth>
+        Загрузка
+      </Button>
+    </>
   );
 };
 

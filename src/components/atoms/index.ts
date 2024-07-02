2ee74ee18670aad25e76a7f2dcab81cda67f5ff1
vladwebdev1402 <mmovlad.kriven.1402@gmail.com>
2024-07-02 16:33:17 +0700

    add navlink

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -4,3 +4,4 @@ export * from './Input';
 export * from './Modal';
 export * from './TabContainer';
 export * from './Tab';
+export * from './NavLink';
--- src/components/pages/MainPage/index.tsx
+++ src/components/pages/MainPage/index.tsx
@@ -1,18 +1,18 @@
-import { Tab, TabContainer } from '@/components/atoms';
+import { NavLink } from '@/components/atoms';
+import { ROUTER_PATHS } from '@/constants';
+import TimeSVG from '@/assets/decorative/time.svg?react';
+import ProfileSVG from '@/assets/decorative/profile.svg?react';
 
 const MainPage = () => {
   return (
-    <TabContainer>
-      <Tab isFullWidth onClick={() => {}}>
-        Большая
-      </Tab>
-      <Tab isFullWidth onClick={() => {}}>
-        Средняя
-      </Tab>
-      <Tab isFullWidth onClick={() => {}}>
-        Маленькая
-      </Tab>
-    </TabContainer>
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+      <NavLink type="footer" to={ROUTER_PATHS.main} icon={<TimeSVG />}>
+        Главная
+      </NavLink>
+      <NavLink type="footer" to={'/adsads'} icon={<ProfileSVG />}>
+        Профиль
+      </NavLink>
+    </div>
   );
 };
 

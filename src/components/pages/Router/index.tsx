3692ebef90451e0760e2d0255335f0ff6c9849ba
vladwebdev1402 <mmovlad.kriven.1402@gmail.com>
2024-07-03 17:54:03 +0700

    add page

--- src/components/pages/Router/index.tsx
+++ src/components/pages/Router/index.tsx
@@ -9,11 +9,13 @@ import { ROUTER_PATHS } from '@/constants';
 
 import { Layout } from '../Layout';
 import { MainPage } from '../MainPage';
+import { ProfilePage } from '../ProfilePage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route element={<Layout />}>
       <Route path={ROUTER_PATHS.main} element={<MainPage />} />
+      <Route path={ROUTER_PATHS.profile} element={<ProfilePage />} />
     </Route>,
   ),
 );

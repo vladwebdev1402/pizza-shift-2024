3a37566a083d863d29520a79894119cbf635675e
vladwebdev1402 <mmovlad.kriven.1402@gmail.com>
2024-07-04 20:46:48 +0700

    add adaptive styles

--- src/components/moleculus/PizzaCard/index.tsx
+++ src/components/moleculus/PizzaCard/index.tsx
@@ -20,23 +20,54 @@ const PizzaCard: FC<PizzaCardProps> = ({ pizza, onPizzaSwitch }) => {
 
   return (
     <div className={style.container}>
-      <div className={style.img}>
-        <img src={API_URL + pizza.img} />
+      <div className={style.desktop_container}>
+        <div className={style.img}>
+          <img src={API_URL + pizza.img} />
+        </div>
+        <div className={style.body}>
+          <div className={style.info}>
+            <Typography variant="h3" tag="h3" className={style.title}>
+              {pizza.name}
+            </Typography>
+            <Typography className={style.description}>
+              {pizza.description}
+            </Typography>
+          </div>
+          <div className={style.footer}>
+            <Typography variant="h3" tag="div" className={style.price}>
+              от {pizza.sizes[0].price} ₽
+            </Typography>
+            <Button onClick={onSwitchClick}>Выбрать</Button>
+          </div>
+        </div>
       </div>
-      <div className={style.info}>
-        <Typography variant="h3" tag="h3" className={style.title}>
-          {pizza.name}
-        </Typography>
-        <Typography className={style.description}>
-          {pizza.description}
-        </Typography>
-      </div>
-      <div className={style.footer}>
-        <Typography variant="h3" tag="div" className={style.price}>
-          от {pizza.sizes[0].price} ₽
-        </Typography>
-        <Button onClick={onSwitchClick}>Выбрать</Button>
-      </div>
+      <button
+        className={style.mobile_container}
+        onClick={() => onPizzaSwitch(pizza.id)}
+      >
+        <div className={style.img}>
+          <img src={API_URL + pizza.img} />
+        </div>
+        <div className={style.body}>
+          <div className={style.info}>
+            <Typography variant="paragraph_16" tag="h3" className={style.title}>
+              {pizza.name}
+            </Typography>
+            <Typography variant="paragraph_12" className={style.description}>
+              {pizza.description}
+            </Typography>
+          </div>
+          <div className={style.footer}>
+            <Typography
+              variant="paragraph_16"
+              tag="div"
+              className={style.price}
+            >
+              от {pizza.sizes[0].price} ₽
+            </Typography>
+          </div>
+        </div>
+      </button>
     </div>
   );
 };
